5d7640b06ddfc2aa2968969139e6b8d784b23735
github username <github email address>
2019-05-16 10:34:17 -0400

    added auth

---
+++ app/controllers/api/v1/users_controller.rb
@@ -0,0 +1,35 @@
+class Api::V1::UsersController < ApplicationController
+
+  def index
+    @users = User.all
+    render json: @users
+  end
+
+  def show
+    @user = User.find(params[:id])
+    render json: @user
+  end
+
+  def create
+    @user = User.create(user_params)
+    if @user.valid?
+      @token = encode_token(user_id: @user.id)
+      render json: { user: UserSerializer.new(@user), jwt: @token}, status: :created
+    else
+      render json: { error: 'failed to create user'}, status: :not_acceptable
+    end
+  end
+
+  def update
+    @user = User.find(params[:id])
+    @user.update(user_params)
+  end
+
+
+  private
+
+  def user_params
+    params.require(:user).permit(:first_name, :last_name, :email, :password)
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resource :users
-  resource :services
-  resource :bookings
+  namespace :api do
+    namespace :v1 do
+      resources :users
+      resource :services
+      resource :bookings
+    end
+  end
 end
